0a44b0845bc52f12dc05594a93ed4eb0fa194876
Jared Braswell <jjb21l@fsu.edu>
2024-01-19 19:13:24 -0500

    Updated with Header and Footer

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,10 +1,10 @@
 import { Navbar, Nav, Container, NavbarToggle } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 
-export const header = () => {
+export const Header = () => {
   return (
     <header>
-        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
                 <Navbar.Brand href="/">ProShop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -19,3 +19,5 @@ export const header = () => {
     </header>
   )
 }
+
+export default Header
