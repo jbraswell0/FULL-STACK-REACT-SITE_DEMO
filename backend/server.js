0ad5083bd88861807c5f76a8ba41d1a2ecd8ce50
Jared Braswell <jjb21l@fsu.edu>
2024-02-01 18:30:51 -0500

    Email & password validation + JSON token & Postman

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import connectDB from './config/db.js';
 import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 
 
 const port = process.env.PORT || 5000;
@@ -12,11 +14,19 @@ connectDB();
 
 const app = express();
 
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('API is running...');
  });
 
  app.use('/api/products', productRoutes);
+ app.use('/api/users', userRoutes);
 
  app.use(notFound);
  app.use(errorHandler);
