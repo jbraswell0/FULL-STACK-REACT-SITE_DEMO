9ca45866c09da5ad362bf8a0016547f556b697e1
Jared Braswell <jjb21l@fsu.edu>
2024-02-16 12:50:23 -0500

    Update Users

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -30,6 +30,7 @@ import OrderListScreen from './screens/OrderListScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -52,7 +53,8 @@ const router = createBrowserRouter(
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
         <Route path="/admin/productlist" element={<ProductListScreen />} />
         <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
-        <Route path='/admin/userList' element={<UserListScreen />} />
+        <Route path='/admin/userlist' element={<UserListScreen />} />
+        <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
       </Route>
     </Route>
   )
