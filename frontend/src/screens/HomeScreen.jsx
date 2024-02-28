92f34d1d8b7a5a79a3129c06eb21bc17d5e15462
Jared Braswell <jjb21l@fsu.edu>
2024-02-27 20:29:25 -0500

    Integrated Pageination

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,10 +4,12 @@ import Product from '../components/Product';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import { useParams } from 'react-router-dom';
 
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
@@ -16,7 +18,7 @@ const HomeScreen = () => {
       ) : error ? (<Message variant='danger'>{ error?.data?.message || error.error }</Message>) : (<>
       <h1>Latest Products</h1>
       <Row>
-          {products.map((product) => (
+          {data.products.map((product) => (
               <Col key={product._id} m={12} md={6} lg={4} xl={3}>
                   <Product product={product} />
               </Col>
